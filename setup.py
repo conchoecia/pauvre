21b6cb0a72345bf74b2d71d9d6db82cef7d73c5f
Darrin Schultz <darrin.schultz91@gmail.com>
2020-02-15 11:36:20 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# pauvre - just a pore PhD student's plotting package
-# Copyright (c) 2016-2017 Darrin T. Schultz. All rights reserved.
+# pauvre 
+# Copyright (c) 2016-2020 Darrin T. Schultz.
 #
 # This file is part of pauvre.
 #
